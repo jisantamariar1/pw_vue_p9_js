6f96e84687eb6090e65fdc1e6d5e4300b5bf97f7
jisantamariar1 <jisantamariar1@uce.edu.ec>
2026-02-12 19:29:54 -0500

    final

--- pw_vue_p9_js/src/clients/ApiVehiculoClient.js
+++ pw_vue_p9_js/src/clients/ApiVehiculoClient.js
@@ -8,7 +8,20 @@ const guardarVehiculo = async (body) => {
     });
     return data;
 }
+const listarVehiculos = async () => {
+    const token = localStorage.getItem("token");
+    const data = await axios.get(`http://localhost:8081/consecionario/api/v1.0/vehiculos`, {
+        headers: {  
+            "Authorization": `Bearer ${token}`
+        }
+    });
+    return data;
+}
 export const guardarVehiculoFachada = async (body) => {
     const data = await guardarVehiculo(body);
+    return data;
+}
+export const listarVehiculosFachada = async () => {
+    const data = await listarVehiculos();
     return data;
 }
